2fdc75975b2ad1783119d6bbcd83183285ae7956
Kabeer <kabeeroad07@gmail.com>
2024-02-04 19:40:58 +0000

    closing the project

--- src/Components/home/upcoming-events.tsx
+++ src/Components/home/upcoming-events.tsx
@@ -1,42 +1,63 @@
 import { CalendarOutlined } from "@ant-design/icons";
-import { Card, List } from "antd";
+import { Badge, Card, List } from "antd";
 import React, { useState } from "react";
 import { Text } from "../text";
+import UpcomingEventsSkeleton from "../skeleton/upcoming-events";
 
 const UpcomingEvents = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  return (
-    <Card
-      style={{ height: "100%" }}
-      headStyle={{ padding: "8px 16px" }}
-      bodyStyle={{ padding: "0 1rem" }}
-      title={
-        <div
-          style={{
-            display: "flex",
-            alignItems: "center",
-            gap: "8px",
-          }}
+    const [isLoading, setIsLoading] = useState(true);
+    return (
+        <Card
+            style={{ height: "100%" }}
+            headStyle={{ padding: "8px 16px" }}
+            bodyStyle={{ padding: "0 1rem" }}
+            title={
+                <div
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        gap: "8px",
+                    }}
+                >
+                    <CalendarOutlined />
+                    <Text size="sm" style={{ marginLeft: "0.7rem" }}>
+                        UpcomingEvents
+                    </Text>
+                </div>
+            }
         >
-          <CalendarOutlined />
-          <Text size="sm" style={{ marginLeft: "0.7rem" }}>
-            UpcomingEvents
-          </Text>
-        </div>
-      }
-    >
-      {isLoading ? (
-        <List
-          itemLayout="horizontal"
-          dataSource={Array.from({ length: 5 }).map((_, index) => ({
-            id: "index",
-          }))}
-        ></List>
-      ) : (
-        <List></List>
-      )}
-    </Card>
-  );
+            {isLoading ? (
+                <List
+                    itemLayout="horizontal"
+                    dataSource={Array.from({ length: 5 }).map((_, index) => ({
+                        id: "index",
+                    }))}
+                    renderItem={() => <UpcomingEventsSkeleton />}
+                />
+
+            ) : (
+                <List
+                    itemLayout="horizontal"
+                    dataSource={[]}
+                    renderItem={(item) => {
+                        const renderDate = getDate(item.startDate, itme.endDate)
+                        return (
+                            <List.Item>
+                                <List.Item.Meta
+                                    avatar={<Badge color={item.color} />}
+                                    title={<Text size="xs">{renderDate}</Text>}
+                                    description={<Text ellipsis={{ tooltip: true }}
+                                        strong>{item.title}
+                                    </Text>
+                                    />
+                        </List.Item>
+                          )
+            }}
+                />
+            }
+        </Card>
+    
+    );
 };
 
 export default UpcomingEvents;
