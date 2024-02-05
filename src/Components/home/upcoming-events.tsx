bafa278df793467dbf5468e1540df2f52470bd8b
Kabeer <kabeeroad07@gmail.com>
2024-02-05 18:46:18 +0000

    adding new recomditions

--- src/Components/home/upcoming-events.tsx
+++ src/Components/home/upcoming-events.tsx
@@ -9,83 +9,94 @@ import { getDate } from "@/utilities/helpers";
 import dayjs from "dayjs";
 
 const UpcomingEvents = () => {
-    const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
 
-    const { data, isLoading: eventsLoading } = useList({
-        resource: 'events',
-        pagination: { pageSize: 5 },
-        sorters: [
-            {
-                field: 'startDate',
-                order: 'asc'
-            }
-        ],
-        filters: [
-            {
-                field: 'startDate',
-                operator: 'gte',
-                value: dayjs().format('YYYY-MM-DD')
-            }
-        ],
-        meta: {
-            gqlQuery: DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY
-        }
-    })
+  const { data, isLoading: eventsLoading } = useList({
+    resource: "events",
+    pagination: { pageSize: 5 },
+    sorters: [
+      {
+        field: "startDate",
+        order: "asc",
+      },
+    ],
+    filters: [
+      {
+        field: "startDate",
+        operator: "gte",
+        value: dayjs().format("YYYY-MM-DD"),
+      },
+    ],
+    meta: {
+      gqlQuery: DASHBORAD_CALENDAR_UPCOMING_EVENTS_QUERY,
+    },
+  });
 
-    return (
-        <Card
-            style={{ height: "100%" }}
-            headStyle={{ padding: "8px 16px" }}
-            bodyStyle={{ padding: "0 1rem" }}
-            title={
-                <div
-                    style={{
-                        display: "flex",
-                        alignItems: "center",
-                        gap: "8px",
-                    }}
-                >
-                    <CalendarOutlined />
-                    <Text size="sm" style={{ marginLeft: "0.7rem" }}>
-                        UpcomingEvents
-                    </Text>
-                </div>
-            }
+  return (
+    <Card
+      style={{ height: "100%" }}
+      headStyle={{ padding: "8px 16px" }}
+      bodyStyle={{ padding: "0 1rem" }}
+      title={
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "8px",
+          }}
         >
-            {isLoading ? (
-                <List
-                    itemLayout="horizontal"
-                    dataSource={Array.from({ length: 5 }).map((_, index) => ({
-                        id: "index",
-                    }))}
-                    renderItem={() => <UpcomingEventsSkeleton />}
+          <CalendarOutlined />
+          <Text size="sm" style={{ marginLeft: "0.7rem" }}>
+            UpcomingEvents
+          </Text>
+        </div>
+      }
+    >
+      {isLoading ? (
+        <List
+          itemLayout="horizontal"
+          dataSource={Array.from({ length: 5 }).map((_, index) => ({
+            id: "index",
+          }))}
+          renderItem={() => <UpcomingEventsSkeleton />}
+        />
+      ) : (
+        <
+          itemLayout="horizontal"
+          dataSource={data?.data || []}
+          renderItem={(item) => {
+            const renderDate = getDate(item.startDate, item.endDate);
+
+            return (
+              <List.Item>
+                <List.Item.Meta
+                  avatar={<Badge color={item.color} />}
+                  title={<Text size="xs">{renderDate}</Text>}
+                  description={
+                    <Text ellipsis={{ tooltip: true }} strong>
+                      {item.title}
+                    </Text>
+                  }
                 />
-
-            ) : (
-                <List
-                    itemLayout="horizontal"
-                    dataSource={data?.data || []}
-                    renderItem={(item) => {
-                        const renderDate = getDate(item.startDate, item.endDate)
-
-                        return (
-                            <List.Item.Meta>
-                                <List.Item.Meta
-                                    avatar={<Badge color={item.color} />}
-                                    title={<Text size="xs">{renderDate}</Text>}
-                                    description={<Text ellipsis={{ tooltip: true }}}
-                                    strong>
-                                    {item.title}
-                                </Text>
-                                    />
-                            </List.Item>
-                        )
-                    }}
-                >
-                </List>
-            )}
-        </Card>
-    )
-}
+              </List.Item>
+            )
+          }}
+        />
+      )}
+      {!isLoading && data?.data.length === 0 && (
+        <span
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "220px",
+          }}
+        >
+          No upcoming events
+        </span>
+      )}
+    </Card>
+  );
+};
 
 export default UpcomingEvents;
